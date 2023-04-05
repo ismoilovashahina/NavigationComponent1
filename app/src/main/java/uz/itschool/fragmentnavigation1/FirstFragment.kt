b7b0d486dfcb079e7cd093baae82596cda169b77
PC903 <ismoilovashahina@gmail.com>
2023-04-05 08:46:20 +0500

    navigation component, take data from login and pass it to the other fragment via object

--- app/src/main/java/uz/itschool/fragmentnavigation1/FirstFragment.kt
+++ app/src/main/java/uz/itschool/fragmentnavigation1/FirstFragment.kt
@@ -12,21 +12,7 @@ import uz.itschool.fragmentnavigation1.data.User
 import uz.itschool.fragmentnavigation1.databinding.FragmentFirstBinding
 
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
 class FirstFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,26 +20,15 @@ class FirstFragment : Fragment() {
     ): View? {
 
         val binding = FragmentFirstBinding.inflate(inflater, container, false)
-        val user = User("Shahina", "123")
-        val name = binding.name.text
-        val password = binding.password.text
 
         binding.t1.setOnClickListener {
+            val name = binding.name.text
+            val password = binding.password.text
+            val user = User(name.toString(), password.toString())
             val bundle = bundleOf("user" to user)
             it.findNavController().navigate(R.id.navigateToSecondFragment, bundle)
         }
         return binding.root
     }
 
-    companion object {
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FirstFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/uz/itschool/fragmentnavigation1/SecondFragment.kt
+++ app/src/main/java/uz/itschool/fragmentnavigation1/SecondFragment.kt
@@ -9,14 +9,8 @@ import androidx.navigation.findNavController
 import uz.itschool.fragmentnavigation1.data.User
 import uz.itschool.fragmentnavigation1.databinding.FragmentFirstBinding
 import uz.itschool.fragmentnavigation1.databinding.FragmentSecondBinding
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
 class SecondFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+
 
 
     override fun onCreateView(
@@ -25,24 +19,11 @@ class SecondFragment : Fragment() {
     ): View? {
         val binding = FragmentSecondBinding.inflate(inflater, container, false)
         val user = arguments?.getSerializable("user") as User
-        val name = param1
-        val password = param2
-        binding.textView.text = name + ' ' + password
+        binding.textView.text = user.name + ' ' + user.password
         binding.t2.setOnClickListener {
             it.findNavController().navigate(R.id.navigateToFirstFragment)
         }
         return binding.root
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
 }
